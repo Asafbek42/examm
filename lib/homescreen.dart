5554297f36c4e34bf9c9d18eadb51aa6b57c8b46
Asafbek42 <asafbek1101@icloud.com>
2025-06-03 09:10:25 +0500

    exam 2.1

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -7,7 +7,6 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    final box = context.watch<GameBox>();
 
     return DefaultTabController(
       length: 4,
